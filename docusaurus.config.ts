b6c3004578226a6f47353e31580591a3ca899249
semyonsinchenko <ssinchenko@apache.org>
2024-08-12 11:00:58 +0200

    Add scala & java syntax highlighting

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -243,6 +243,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['scala', 'java'],
     },
   } satisfies Preset.ThemeConfig,
 };
